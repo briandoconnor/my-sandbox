ee46dab5b01481211e253a56e627b88ce9e75780
Brian O'Connor <briandoconnor@gmail.com>
2015-06-24 15:42:32 -0400

    Updating script

--- 20150526_fixing_pancancer_metadata_problem_sergei/scripts/generate_fix.pl
+++ 20150526_fixing_pancancer_metadata_problem_sergei/scripts/generate_fix.pl
@@ -4,13 +4,40 @@ use Data::Dumper;
 
 if (!-e "donor_p_150526020206.jsonl.gz") { system("wget http://pancancer.info/gnos_metadata/2015-05-26_02-02-06_UTC/donor_p_150526020206.jsonl.gz"); }
 
+my $donors = {};
+
+# first, harvest the donor IDs of what we need to look at
+foreach my $dir (glob ("data/*")) {
+  #print "$dir\n";
+  if (-d "$dir") {
+    $dir =~ /data\/(\S+)/;
+    #print "DIR: $dir\n";
+    $donors->{$1} = 1;
+  } else { print "NOT DIR: $dir\n"; }
+}
+
+# fixes, a data structure to contain the mapped information to fix the original files
+
 open IN, "gunzip -c donor_p_150526020206.jsonl.gz | " or die "Can't open donor_p_150526020206.jsonl.gz";
 
 while (<IN>) {
-  if (/ce799e7b-30e7-44a5-a185-3e50d5e059ef/) {
-    my $jd = decode_json ($_);
-    print Dumper $jd;
+  foreach my $key (keys %{$donors}) {
+    #print "KEY: $key\n";
+    if (/$key/) {
+      delete $donors->{$key};
+      my $jd = decode_json ($_);
+      print Dumper $jd;
+      die;
+
+      # for the aligned normal
+
+      # for the aligned tumor
+
+    }
   }
 }
 
 close IN;
+
+# now that I've collected all the mapping info I need, open up the original files
+# and edit them to correct the mapping issue.
