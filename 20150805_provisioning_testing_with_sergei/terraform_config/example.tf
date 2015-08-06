3330d013ae0d368191fbd7c0d704e9c68a0a856c
Brian O'Connor <briandoconnor@gmail.com>
2015-08-06 16:33:39 -0400

    Adding state file

---
+++ 20150805_provisioning_testing_with_sergei/terraform_config/example.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+    access_key = "${var.access_key}"
+    secret_key = "${var.secret_key}"
+    region = "us-east-1"
+}
+
+resource "aws_instance" "example" {
+    ami = "ami-408c7f28"
+    tags {
+       Name = "BrianTerraformTest"
+       KEEP = "BRIAN"
+    }
+    instance_type = "t1.micro"
+}
