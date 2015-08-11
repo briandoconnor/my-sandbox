4385c1e4baf35fd381c5e45372d43be5ded14a0c
Brian O'Connor <briandoconnor@gmail.com>
2015-08-11 15:53:23 -0400

    Adding rheos profile

---
+++ 20150805_provisioning_testing_with_sergei/terraform_hadoop_rheos_config/rheos.tf
@@ -0,0 +1,103 @@
+variable "access_key" {}
+variable "secret_key" {}
+variable "ami_id" {}
+variable "name_key" {}
+variable "vol_size" {}
+variable "ssh_key" {}
+
+provider "aws" {
+    access_key = "${var.access_key}"
+    secret_key = "${var.secret_key}"
+    region = "us-east-1"
+}
+
+resource "aws_instance" "ambari" {
+    ami = "${var.ami_id}"
+    tags {
+       Name = "ambari"
+       KEEP = "${var.name_key}"
+    }
+    instance_type = "m1.medium"
+    key_name = "${var.ssh_key}"
+    root_block_device {
+        volume_size = "${var.vol_size}"
+    }
+}
+
+resource "aws_instance" "zoo-name" {
+    ami = "${var.ami_id}"
+    tags {
+       Name = "zoo-name"
+       KEEP = "${var.name_key}"
+    }
+    instance_type = "m1.medium"
+    key_name = "${var.ssh_key}"
+    root_block_device {
+        volume_size = "${var.vol_size}"
+    }
+}
+
+resource "aws_instance" "spark-store01" {
+    ami = "${var.ami_id}"
+    tags {
+       Name = "spark-store01"
+       KEEP = "${var.name_key}"
+    }
+    instance_type = "m1.large"
+    key_name = "${var.ssh_key}"
+    root_block_device {
+        volume_size = "${var.vol_size}"
+    }
+}
+
+resource "aws_instance" "spark-store02" {
+      ami = "${var.ami_id}"
+      tags {
+         Name = "spark-store02"
+         KEEP = "${var.name_key}"
+      }
+      instance_type = "m1.large"
+      key_name = "${var.ssh_key}"
+      root_block_device {
+          volume_size = "${var.vol_size}"
+      }
+}
+
+resource "aws_instance" "spark-store03" {
+      ami = "${var.ami_id}"
+      tags {
+         Name = "spark-store03"
+         KEEP = "${var.name_key}"
+      }
+      instance_type = "m1.large"
+      key_name = "${var.ssh_key}"
+      root_block_device {
+          volume_size = "${var.vol_size}"
+      }
+}
+
+resource "aws_instance" "kafka00" {
+      ami = "${var.ami_id}"
+      tags {
+         Name = "kafka00"
+         KEEP = "${var.name_key}"
+      }
+      instance_type = "m1.large"
+      key_name = "${var.ssh_key}"
+      root_block_device {
+          volume_size = "${var.vol_size}"
+      }
+}
+
+resource "aws_instance" "kafka01" {
+      ami = "${var.ami_id}"
+      tags {
+         Name = "kafka01"
+         KEEP = "${var.name_key}"
+      }
+      instance_type = "m1.large"
+      key_name = "${var.ssh_key}"
+      root_block_device {
+          volume_size = "${var.vol_size}"
+      }
+}
