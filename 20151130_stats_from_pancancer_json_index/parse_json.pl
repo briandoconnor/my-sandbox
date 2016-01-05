26150910e47cb719db0cdf0fa248728bb04f9465
Brian O'Connor <briandoconnor@gmail.com>
2016-01-05 17:41:34 -0500

    adding info about variant calls

--- 20151130_stats_from_pancancer_json_index/parse_json.pl
+++ 20151130_stats_from_pancancer_json_index/parse_json.pl
@@ -18,6 +18,8 @@ while(<STDIN>) {
   my $donor = decode_json($_);
   $d->{total_donors}++;
   $d->{"$type\_donors"}++;
+
+  # the alignments
   if (defined($donor->{gnos_repos_with_complete_alignment_set})) {
     $d->{donors_with_gnos_complete}++;
     $d->{"$type\_donors_with_gnos_complete"}++;
@@ -26,6 +28,19 @@ while(<STDIN>) {
       $d->{"$type\_donors_with_gnos_complete_not_empty"}++;
       foreach my $gnos (@{$donor->{gnos_repos_with_complete_alignment_set}}) {
         $d->{counts}{$gnos}{$type}++;
+      }
+    }
+  }
+
+  # the variant calls
+  if (defined($donor->{variant_calling_results})) {
+    $d->{"$type\_donors_with_variant_calling_results"}++;
+    if (scalar(keys %{$donor->{variant_calling_results}}) == 3) {
+      $d->{"$type\_donors_with_complete_variant_calling_results"}++;
+    }
+    foreach my $var_call_type (keys %{$donor->{variant_calling_results}}) {
+      foreach my $var_gnos_loc (@{$donor->{variant_calling_results}{$var_call_type}{gnos_repo}}) {
+        $d->{var_counts}{$var_gnos_loc}{$type}{$var_call_type}++;
       }
     }
   }
