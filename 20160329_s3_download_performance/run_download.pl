8d7e543b33cf97722934b82d0a5d547e81415181
Brian O'Connor <boconnor@Brians-Mac-Pro.local>
2016-03-30 12:03:28 -0700

    update

--- 20160329_s3_download_performance/run_download.pl
+++ 20160329_s3_download_performance/run_download.pl
@@ -1,7 +1,10 @@
 use strict;
 
-# first sleep randomly 10-120 seconds
-sleep (int(rand(111)) + 10);
+my ($repeats, $instance_type) = @ARGV;
+
+if ($instance_type eq '') {
+  $instance_type = "unknown";
+}
 
 # next setup directory
 system ("mkdir -p /mnt/data");
@@ -16,30 +19,50 @@ while (<IN>) {
   }
 }
 close IN;
-# randomly select one for download
-my $max = scalar(@d);
-my $index = int(rand($max));
-my $sample = $d[$index];
-my @tokens = split /\|/, $sample;
-my $oid = $tokens[0];
-my $size = $tokens[1];
 
-# create touch file and upload
-my $start_time = `date +\%s`;
-chomp $start_time;
-open OUT, ">/home/ubuntu/$oid.tsv" or die;
-print OUT "OID\t$oid\nSIZE\t$size\nSTART\t$start_time\n";
-close OUT;
+my $curr = $repeats;
+if ($curr <= 0) { $curr = 1; $repeats = 1; }
+while($curr > 0) {
 
-# do download
-my $status = system("/home/ubuntu/icgc-storage-client-1.0.13/bin/icgc-storage-client download --object-id $oid --output-dir /mnt/data/");
+  # loop
+  $curr--;
 
-# update touch file and upload
-my $end_time = `date +\%s`;
-chomp $end_time;
-open OUT, ">>/home/ubuntu/$oid.tsv" or die;
-print OUT "END\t$end_time\nEXITCODE\t$status";
-close OUT;
+  # clean
+  system ("rm -rf /mnt/data/*");
 
-# upload to s3
-system ("aws s3 cp /home/ubuntu/$oid.tsv s3://oconnor-test-bucket/icgc-storage-download-testing/");
+  # first sleep randomly 10-120 seconds
+  sleep (int(rand(111)) + 10);
+
+  # randomly select one for download
+  my $max = scalar(@d);
+  my $index = int(rand($max));
+  my $sample = $d[$index];
+  my @tokens = split /\|/, $sample;
+  my $oid = $tokens[0];
+  my $size = $tokens[1];
+
+  # create touch file and upload
+  my $start_time = `date +\%s`;
+  chomp $start_time;
+  open OUT, ">/home/ubuntu/$oid.tsv" or die;
+  print OUT "OID\t$oid\nINSTANCE\t$instance_type\nSIZE\t$size\nSTART\t$start_time\n";
+  close OUT;
+
+  # upload to s3
+  system ("aws s3 cp /home/ubuntu/$oid.tsv s3://oconnor-test-bucket/icgc-storage-download-testing/$oid.$repeats.$instance_type.running.tsv");
+
+  # do download
+  my $status = system("/home/ubuntu/icgc-storage-client-1.0.13/bin/icgc-storage-client download --object-id $oid --output-dir /mnt/data/");
+
+  # update touch file and upload
+  my $end_time = `date +\%s`;
+  chomp $end_time;
+  open OUT, ">>/home/ubuntu/$oid.tsv" or die;
+  print OUT "END\t$end_time\nEXITCODE\t$status";
+  close OUT;
+
+  # upload to s3
+  system ("aws s3 rm s3://oconnor-test-bucket/icgc-storage-download-testing/$oid.$repeats.$instance_type.running.tsv");
+  system ("aws s3 cp /home/ubuntu/$oid.tsv s3://oconnor-test-bucket/icgc-storage-download-testing/$oid.$repeats.$instance_type.finished.tsv");
+
+}
