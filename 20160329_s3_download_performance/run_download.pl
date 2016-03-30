90d9467e91eec51698dbb84b39fdb0b1bd8c9778
Brian O'Connor <boconnor@Brians-Mac-Pro.local>
2016-03-29 20:05:44 -0700

    update

--- 20160329_s3_download_performance/run_download.pl
+++ 20160329_s3_download_performance/run_download.pl
@@ -10,8 +10,8 @@ system ("mkdir -p /mnt/data");
 my @d = ();
 open IN, "</home/ubuntu/icgc-storage-client-1.0.13/manifest.txt" or die;
 while (<IN>) {
-  my @a = split /\s/;
-  if (!/repo_code/) {
+  my @a = split /\t/;
+  if (!/^repo_code/) {
     push @d, "$a[2]|$a[5]";
   }
 }
@@ -20,7 +20,7 @@ close IN;
 my $max = scalar(@d);
 my $index = int(rand($max));
 my $sample = $d[$index];
-my @tokens = split "|", $sample;
+my @tokens = split /\|/, $sample;
 my $oid = $tokens[0];
 my $size = $tokens[1];
 
