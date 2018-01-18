321b39fe6c6aae36c80414de9e457d5c47b98458
Brian O'Connor <briandoconnor@gmail.com>
2018-01-17 18:41:37 -0800

    adding

--- 20180117_topmed_data/gsutil_to_s3_upload.py
+++ 20180117_topmed_data/gsutil_to_s3_upload.py
@@ -4,8 +4,9 @@ with open('TOPMed.public_samples.manifest.2017.11.30.txt') as f:
     for line in f:
         tokens = line.split('\t')
         print(line)
-
-        call (['gsutil', '-u', 'platform-dev-178517', 'cp',  tokens[3], '.'])
-        call (['gsutil', '-u', 'platform-dev-178517', 'cp',  tokens[4], '.'])
-        call (['aws', 's3', 'cp', '*.cram*',  's3://cgp-commons-public/topmed_open_access'])
+        #call (['gsutil', '-u', 'platform-dev-178517', 'cp',  tokens[3], '.'], shell=True)
+        print ('gsutil -u platform-dev-178517 cp '+tokens[4].rstrip()+' . ')
+        call ('gsutil -u platform-dev-178517 cp '+tokens[4].rstrip()+' . ', shell=True)
+        call ('aws s3 cp *.cram* s3://cgp-commons-public/topmed_open_access/', shell=True)
+        call ('rm *.cram*', shell=True)
 f.closed
